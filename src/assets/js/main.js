af7c8b71eb18c8c2f84ed810742d54f1b2942e63
Klimovich Yauheniya <zh.shuravina@gmail.com>
2022-01-20 15:47:45 +0300

    add anchor in js commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,3 +19,19 @@ function burgerMenu() {
 }
 
 burgerMenu();
+
+
+
+let anchors = document.querySelectorAll("a[href*='#']");
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        let blockID = anchor.getAttribute('href');
+        document.querySelector(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
